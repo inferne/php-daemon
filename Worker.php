6b8b12109e6ea6e3e6ddad25bdc4bad42d87cfd9
liyunfei <liyunfei@360.cn>
2018-05-31 19:30:28 +0800

    lazy load queue

--- Worker.php
+++ Worker.php
@@ -34,7 +34,9 @@ class Worker
     
     public function init(){
         //$this->log_file = 'Worker.log';
-        $this->queue = new Queue();
+        $this->queue = function(){
+            return new Queue();//延迟加载
+        };
     }
     
     /**
